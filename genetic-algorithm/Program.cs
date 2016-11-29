a801498f6716f57e81b0a0f48fb79c8e17fdf909
Kamil Kapka <kamil.kapka@hotmail.com>
2016-11-29 21:47:49 +0100

    Made a program sceleton

--- genetic-algorithm/Program.cs
+++ genetic-algorithm/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using genetic_algorithm.crossing;
+using genetic_algorithm.mutation;
+using genetic_algorithm.population;
+using genetic_algorithm.selection;
 
 namespace genetic_algorithm
 {
@@ -9,6 +12,12 @@ namespace genetic_algorithm
             string selectionType=args[0];
             string crossoverType=args[1];
             string mutationType=args[2];
+
+            Population population = new Population();
+
+            population = new SelectionContext(new SelectionFactory().getSelectionMethod(selectionType)).executeStrategy(population);
+            population = new CrossoverContext(new CrossoverFactory().getCrossoverMethod(crossoverType)).executeStrategy(population);
+            population = new MutationContext(new MutationFactory().getMutationMethod(mutationType)).executeStrategy(population);
         }
     }
 }
